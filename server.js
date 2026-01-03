09b496237fd5c77fe538647efc05f96e94653c6e
MuthuKumar6 <muthukumar143412@gmail.com>
2026-01-03 15:07:50 +0530

    Commit Second Changes

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from Node.js backend 🚀" });
 });
 
+// New Test API – Great for health checks or testing deployment
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "Server is healthy and running! - API 2",
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
 app.listen(5000, () => {
   console.log("Backend running on port 5000");
 });
